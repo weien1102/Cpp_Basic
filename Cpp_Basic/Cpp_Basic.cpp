4bde7e4fd7a49e2ad98660b6e81a08664fe8ba0b
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-02-23 16:31:01 +0800

    I/O manip

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,36 @@
 ﻿#include <iostream>
+#include <iomanip>
+#include <bitset>
+using namespace std;
 
+const double PI = 3.1415926;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int number = 250;
+    cout << "number = " << number << endl;
+
+    cout << "number (hex) = " << hex << number << endl;
+    cout << "number (oct) = " << oct << number << endl;
+    cout << "number (dec) = " << dec << number << endl;
+    cout << "number (binary) = " << bitset<16>(number) << endl;
+
+    cout << setfill('*') << setw(10) << number << endl;
+
+    char a0 = 'A'; 
+    char a1 = 65;
+    cout << "a0 = " << a0 << endl;
+    cout << "a1 = " << a1 << endl;
+
+    const int WIDTH = 15;
+
+    cout << setfill(' ');
+    cout.setf(ios::right);
+    //cout << right;
+    cout << setw(WIDTH / 2) << "半徑" << setw(WIDTH) << "圓周長" << endl;
+    cout.setf(ios::fixed);
+    for (double radius = 1; radius <= 6; radius += 0.5) {
+        cout << setprecision(1) << setw(WIDTH / 2) << radius << setprecision(6) << setw(WIDTH) << 2 * PI * radius << endl;
+    }
+
+    return 0;
 }
