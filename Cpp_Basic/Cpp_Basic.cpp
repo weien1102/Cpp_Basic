8af5cf2564fb21052e033c218410c46a43c14b0f
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-05 22:26:03 +0800

    prime

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // 判斷是否為質數
 bool isPrime(int n) {
 	if (n <= 1) return false;
-    for (int i = 2; i < n; i++) {
+	for (int i = 2; i < n; i++) {
 		if (n % i == 0) return false;
 	}
 	return true;
@@ -12,13 +12,13 @@ bool isPrime(int n) {
 
 int main()
 {
-    //列出小於等於n的所有質數
+	//列出小於等於n的所有質數
 
-    int n;
-    cout << "輸入一個數值n: ";
-    cin >> n;
-    cout << "小於等於" << n << "的質數有: ";
-    for (int i = 1; i <= n; i++) {
-        if (isPrime(i)) cout << i << endl;
-    }
+	int n;
+	cout << "輸入一個數值n: ";
+	cin >> n;
+	cout << "小於等於" << n << "的質數有: " << endl;
+	for (int i = 1; i <= n; i++) {
+		if (isPrime(i)) cout << i << endl;
+	}
 }
