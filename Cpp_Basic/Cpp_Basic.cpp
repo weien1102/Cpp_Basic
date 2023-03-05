2196254c86736548f6c8de275debfd3f87e2efec
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-05 22:14:25 +0800

    statements

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,40 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	int sum = 0;
+
+	// 計算1加到n
+	// for loop
+	cout << "請輸入一個數值n: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	cout << "使用for loop: " << "1至" << n << "的總和= " << sum << endl;
+
+	// while loop
+	int i = 1;
+	sum = 0;
+	while (i <= n) {
+		sum += i;
+		i++;
+	}
+	cout << "使用while loop: " << "1至" << n << "的總和= " << sum << endl;
+
+	// do-while loop
+	i = 1;
+	sum = 0;
+	do {
+		sum += i;
+		i++; 
+		} while (i <= n);
+	cout << "使用do-while loop: " << "1至" << n << "的總和= " << sum << endl;
+
+	// 輸// C++// if-else
+
+	// 給定一個分數，輸出對應的等第
 }
