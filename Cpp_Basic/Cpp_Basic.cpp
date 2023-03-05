5fe6748ce3975fe1f0fee3df1c54dafc0f945bdd
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-05 22:26:50 +0800

    prime

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 // 判斷是否為質數
-bool isPrime(int n) {
-	if (n <= 1) return false;
-	for (int i = 2; i < n; i++) {
-		if (n % i == 0) return false;
+bool isPrime(int p) {
+	if (p <= 1) return false;
+	for (int i = 2; i < p; i++) {
+		if (p % i == 0) return false;
 	}
 	return true;
 }
