b14af84935f93715d9fe602fe51368401d6dc223
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-06 14:22:51 +0800

    function

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -11,20 +11,7 @@ int main()
 	cout << "請輸入分數: ";
 	cin >> score;
 
-	if (score >= 90) {
-		grade = "A";
-	}
-	else if (score >= 80) {
-		grade = "B";
-	}
-	else if (score >= 70) {
-		grade = "C";
-	}
-	else if (score >= 60) {
-		grade = "D";
-	}
-	else grade = "E";
-
+	grade = convertScore(score);
 	//if (score >= 60) isPass = "及格";
 	//else isPass = "不及格";
 	isPass = (score >= 60) ? "及格" : "不及格";
@@ -33,4 +20,44 @@ int main()
 	cout << "等第為: " << grade << endl;
 	cout << "是否及格: " << isPass << endl;
 	(score >= 60) ? cout << "恭喜成績及格" << endl : cout << "成績不及格，請繼續努力" << endl;
+}
+
+string convertScore(int s) {
+	string grade;
+
+	// if else
+	//if (score >= 90) {
+	//	grade = "A";
+	//}
+	//else if (score >= 80) {
+	//	grade = "B";
+	//}
+	//else if (score >= 70) {
+	//	grade = "C";
+	//}
+	//else if (score >= 60) {
+	//	grade = "D";
+	//}
+	//else grade = "E";
+
+	// switch case
+	switch (s / 10) {
+		case 10:
+		case 9:
+			grade = "A";
+			break;
+		case 8:
+			grade = "B";
+			break;
+		case 7:
+			grade = "C";
+			break;
+		case 6:
+			grade = "D";
+			break;
+		default:
+			grade = "E";
+			break;
+	}
+	return grade;
 }
