4b7ba966a0a48c436b5e6fa5ad6821b37a6db40a
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-06 14:26:06 +0800

    function

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -2,26 +2,6 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-	int score;
-	string grade;
-	string isPass;
-	string message;
-	cout << "請輸入分數: ";
-	cin >> score;
-
-	grade = convertScore(score);
-	//if (score >= 60) isPass = "及格";
-	//else isPass = "不及格";
-	isPass = (score >= 60) ? "及格" : "不及格";
-
-	cout << "分數為: " << score << endl;
-	cout << "等第為: " << grade << endl;
-	cout << "是否及格: " << isPass << endl;
-	(score >= 60) ? cout << "恭喜成績及格" << endl : cout << "成績不及格，請繼續努力" << endl;
-}
-
 string convertScore(int s) {
 	string grade;
 
@@ -61,3 +41,29 @@ string convertScore(int s) {
 	}
 	return grade;
 }
+
+void outputResult(int score) {
+	string grade,isPass;
+	grade = convertScore(score);
+	//if (score >= 60) isPass = "及格";
+	//else isPass = "不及格";
+	isPass = (score >= 60) ? "及格" : "不及格";
+
+	cout << "分數為: " << score << endl;
+	cout << "等第為: " << grade << endl;
+	cout << "是否及格: " << isPass << endl;
+	(score >= 60) ? cout << "恭喜成績及格" << endl : cout << "成績不及格，請繼續努力" << endl;
+}
+
+int main()
+{
+	int score;
+	string grade;
+	string isPass;
+	string message;
+	cout << "請輸入分數: ";
+	cin >> score;
+
+	outputResult(score);
+}
+
