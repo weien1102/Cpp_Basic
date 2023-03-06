22ff054094ed585a61157a925505d983cc501bfd
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-06 09:34:17 +0800

    loop

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,16 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n; 
+    int sum = 0;
+    
+    cout << "請輸入一個數值n: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    cout << "使用for loop: " << "i至" << n << "的總和" << sum << endl;
 }
