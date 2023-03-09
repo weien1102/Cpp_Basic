b1ca4b3de2b61b2cce66c7d5e6e01ee88bc8f64b
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-09 15:33:39 +0800

    loop

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,37 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n;
+    int sum = 0;
+
+    //計算從1加到n的總和
+    cout << "請輸入一個數值n: ";
+    cin >> n;
+
+    // for loop
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+        if (i == 3) break;
+    }
+    cout << "使用for loop: " << "1至" << n << "的總和為= " << sum << endl;
+
+    // while loop
+    int i = 1;
+    sum = 0;
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+    cout << "使用while loop: " << "1至" << n << "的總和為= " << sum << endl;
+
+    // do-while loop
+    i = 1;
+    sum = 0;
+    do {
+        sum += i;
+        i++;
+    } while (i <= n);
+    cout << "使用do-while loop: " << "1至" << n << "的總和為= " << sum << endl;
 }
