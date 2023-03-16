dbd04db7e5531fb1620a70e0b988b5ddb56650f3
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-16 15:40:44 +0800

    function overloading

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,42 @@
 ﻿#include <iostream>
+using namespace std;
+
+int absolute(int var) {
+    if (var < 0) var = -var;
+    return var;
+}
+
+double absolute(double var) {
+    if (var < 0) var = -var;
+    return var;
+}
+
+void display(int var) {
+    cout << "整數為：" << var << endl;
+}
+
+void display(double var) {
+    cout << "浮點數為：" << var << endl;
+}
+
+void display(int var1, double var2) {
+    cout << "整數為：" << var1 << endl;
+    cout << "浮點數為：" << var2 << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a;
+    double b;
+
+    cout << "請輸入整數值a :";
+    cin >> a;
+    cout << "請輸入浮點數值b :";
+    cin >> b;
+    cout << a << "的絕對值為 " << absolute(a) << endl;
+    cout << b << "的絕對值為 " << absolute(b) << endl;
+
+    display(a);
+    display(b);
+    display(a, b);
 }
