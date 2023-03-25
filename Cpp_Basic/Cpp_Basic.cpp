30bacde0ae6275ea60877f5882f71020008e6f60
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-25 10:54:46 +0800

    pointer

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,44 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int var = 20;
+	//int* ip;
+	//int* ip = 0;
+	//int* ip = NULL;
+	int* ip = nullptr;
+	ip = &var; // & 取址運算子
+
+	cout << "var = " << var << endl;
+	cout << "&var = " << &var << endl;
+	cout << "ip = " << ip << endl;
+	cout << "*ip = " << *ip << endl; // * 取值運算子
+	cout << endl;
+
+	int** ipp;
+	ipp = &ip;
+
+	cout << "var = " << var << endl;
+	cout << "ip (i.e. &var) = " << ip << endl;
+	cout << "ipp (i.e. &ip) = " << ipp << endl;
+	cout << "*ipp = " << *ipp << endl;
+	cout << "**ipp = " << **ipp << endl;
+	cout << endl;
+
+	const int N = 5;
+	int a[N] = { 10, 20, 30, 40, 50 }; //整數陣列
+	int* ptr = nullptr;
+	ptr = a;
+
+	cout << "a[3] = " << a[3] << endl;
+	cout << "*(ptr+3) = " << *(ptr + 3) << endl;
+
+	for (int i = 0; i < N; i++) {
+		cout << "a[" << i << "] = " << a[i] << endl;
+		cout << "第" << i << "個指標 = " << ptr << endl;
+		cout << "第" << i << "個指標的內容 = " << *ptr << endl;
+		ptr++;
+	}
+
 }
