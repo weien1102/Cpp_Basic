531db98c4ea40f3274ad4ada20bf8d88ce5c3473
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-25 14:27:54 +0800

    pointer

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -24,4 +24,26 @@ int main()
 	cout << "*ip = " << *ip << endl;
 	cout << "*ipp = " << *ipp << endl;
 	cout << "**ipp = " << **ipp << endl;
+	cout << endl;
+
+	const int N = 5;
+	int a[N] = { 0, 1, 2, 3, 4 };
+	int* ptr = nullptr;
+	ptr = a;
+
+	cout << "a[3] = " << a[3] << endl;
+	cout << "*(ptr+3) = " << *(ptr + 3) << endl;
+	cout << endl;
+	//cout << "*(ptr+5) = " << *(ptr + 5) << endl; // 超出陣列範圍
+
+	for (int i = 0; i < N; i++) {
+		cout << "a[" << i << "] = " << a[i] << endl;
+		cout << "第" << i << "個指標 = " << ptr << endl;
+		cout << "第" << i << "個指標的內容 = " << *ptr << endl;
+		ptr++;
+	}
+
+	string str = "Hello World";
+	char* ptr_str = &str[0];
+	cout << "*(ptr_str+6) = " << *(ptr_str + 6) << endl;
 }
