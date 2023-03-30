5d539f5439822b055bc3714da327dc17072c66a9
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2023-03-30 16:35:06 +0800

    reference

--- Cpp_Basic/Cpp_Basic.cpp
+++ Cpp_Basic/Cpp_Basic.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
+using namespace std;
+
+void swap(int& x, int& y) {
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int i = 5;
+    int& i_ref = i;
+
+    cout << "value of i = " << i << endl;
+    cout << "value of i reference = " << i_ref << endl;
+    cout << endl;
+
+    i_ref = 10;
+    cout << "change i_ref=10" << endl;
+    cout << "value of i = " << i << endl;
+    cout << "value of i reference = " << i_ref << endl;
+    cout << endl;
+
+    int a = 100;
+    int b = 200;
+    cout << "swap前，a = " << a << endl;
+    cout << "swap前，b = " << b << endl;
+    swap(a, b);
+    cout << "swap後，a = " << a << endl;
+    cout << "swap後，b = " << b << endl;
 }
